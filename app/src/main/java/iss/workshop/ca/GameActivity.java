3c5580009c009364436c0557f06eaca8714e4572
GrimPerspective <45945593+GrimPerspective@users.noreply.github.com>
2021-07-14 12:08:15 +0800

    Added timer

--- app/src/main/java/iss/workshop/ca/GameActivity.java
+++ app/src/main/java/iss/workshop/ca/GameActivity.java
@@ -6,6 +6,10 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.Chronometer;
 import android.widget.TextView;
 
 import java.io.IOException;
@@ -18,11 +22,33 @@ public class GameActivity extends AppCompatActivity {
     ArrayList<String> imgSelectedUrl;
     private static final int imgForGame = 6;
 
-    TextView timerText;
-    boolean timerStarted = false;
-    Double time = 0.0;
-    TimerTask timerTask;
-    Timer timer;
+//    TextView timerText;
+//    boolean timerStarted = false;
+//    long startTime = 0;
+//    static long millis;
+//    static long finalTime;
+//    static int seconds;
+//    static int minutes;
+//    Handler timerHandler = new Handler();
+//    Runnable timerRunnable = new Runnable() {
+//        @Override
+//        public void run() {
+//            millis = 0;
+//            finalTime = 0;
+//            seconds = 0;
+//            minutes = 0;
+//
+//            millis = System.currentTimeMillis() - startTime;
+//            seconds = (int) (millis / 1000);
+//            minutes = seconds / 60;
+//            seconds = seconds % 60;
+//
+//            timerText.setText(String.format("Time taken %d:%02d", minutes, seconds));
+//            timerHandler.postDelayed(this, 500);
+//        }
+//    };
+
+
 
 
     @Override
@@ -30,46 +56,49 @@ public class GameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
 
-//        timerText = (TextView)findViewById(R.id.txt_timing);
-//
-//        timer = new Timer();
-//
-//        private void startTime(){
-//            timer = new TimerTask() {
-//                @Override
-//                public void run() {
-//                    time++;
-//                    timerText.setText(getTimerText);
-//                }
-//            };
-//        }
-//
-//        private String getTimerText(){
-//            int rounded = (int) Math.round(time);
-//            int seconds = ((rounded % 86400) % 3600 % 60);
-//            int minutes = ((rounded % 86400) % 3600 / 60);
-//            int hours = ((rounded % 86400) / 3600);
-//            return formatTime(seconds, minutes, hours);
-//        }
-//
-//        private String formatTime(int seconds, int minutes, int hours)
-
-
-
         //get the selected 6 pics
         Intent intent = getIntent();
         imgSelectedUrl = intent.getStringArrayListExtra("imgSelectedUrl");
-
-        for(String imgurl : imgSelectedUrl) {
-            try {
-                URL url= new URL(imgurl);
-                Bitmap image = BitmapFactory.decodeStream(url.openConnection().getInputStream());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-
         setContentView(R.layout.activity_game);
+
+//        for (String imgurl : imgSelectedUrl) {
+//            try {
+//                URL url = new URL(imgurl);
+//                Bitmap image = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+
+        Chronometer timerChronometer;
+        timerChronometer = (Chronometer)findViewById(R.id.chronometer);
+        timerChronometer.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener(){
+            @Override
+            public void onChronometerTick(Chronometer cArg) {
+                long time = SystemClock.elapsedRealtime() - cArg.getBase();
+                int h = (int)(time /3600000);
+                int m = (int)(time - h*3600000)/60000;
+                int s= (int)(time - h*3600000- m*60000)/1000 ;
+                String hh = h < 10 ? "0"+h: h+"";
+                String mm = m < 10 ? "0"+m: m+"";
+                String ss = s < 10 ? "0"+s: s+"";
+                cArg.setText(hh+":"+mm+":"+ss);
+            }
+        });
+        timerChronometer.setBase(SystemClock.elapsedRealtime());
+        timerChronometer.start();
+
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+//        timerText = findViewById(R.id.txt_timing);
+//        startTime = System.currentTimeMillis();
+//        timerHandler.postDelayed(timerRunnable, 0);
+
+
+    }
+
 }
